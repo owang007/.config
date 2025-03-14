19c68706b28692a65aa0a798da7a22046a19fcc5
Oscar Wang <oscarw6926@gmail.com>
2025-03-13 20:49:34 -0700

    Added gitsigns and fixed lsp

--- lua/oscar/plugins/lsp-config.lua
+++ lua/oscar/plugins/lsp-config.lua
@@ -3,9 +3,10 @@ return {
 	branch = "v3.x",
 	dependencies = {
 		{ "neovim/nvim-lspconfig" },
-    { "nvimtools/none-ls.nvim" },
+		{ "nvimtools/none-ls.nvim" },
+		{ "nvimtools/none-ls-extras.nvim" },
 		{ "williamboman/mason.nvim" },
-    { "williamboman/mason-null-ls.nvim" },
+		{ "williamboman/mason-null-ls.nvim" },
 		{ "williamboman/mason-lspconfig.nvim" },
 		{ "hrsh7th/cmp-nvim-lsp" },
 		{ "hrsh7th/nvim-cmp" },
@@ -17,6 +18,10 @@ return {
 		vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 		vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
 
+		-- Add cmp_nvim_lsp capabilities settings to lspconfig
+		-- This should be executed before you configure any language server
+		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			desc = "LSP actions",
 			callback = function(event)
@@ -24,7 +29,6 @@ return {
 
 				-- these will be buffer-local keybindings
 				-- because they only work if you have an active language server
-
 				vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
 				vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
 				vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
@@ -38,19 +42,21 @@ return {
 			end,
 		})
 
-		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-		local default_setup = function(server)
-			require("lspconfig")[server].setup({
-				capabilities = lsp_capabilities,
-			})
-		end
-
 		require("mason").setup({})
 		require("mason-lspconfig").setup({
-			ensure_installed = {},
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+				"tsserver",
+				"clangd",
+				"gopls",
+			},
 			handlers = {
-				default_setup,
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilitites = lsp_capabilities,
+					})
+				end,
 			},
 		})
 
@@ -62,26 +68,49 @@ return {
 			},
 			mapping = cmp.mapping.preset.insert({
 				-- Enter key confirms completion item
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 
 				-- Ctrl + space triggers completion menu
 				["<C-Space>"] = cmp.mapping.complete(),
+
+				-- Scroll up and down in the completion documentation
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
 			}),
 			snippet = {
 				expand = function(args)
 					require("luasnip").lsp_expand(args.body)
 				end,
 			},
-    })
+		})
 
-    require("null-ls").setup({
-      sources = {}
-    })
+		local null_ls = require("null-ls")
 
-    require("mason-null-ls").setup({
-      ensure_installed = nil,
-      automatic_installation = true,
-      handlers = {}
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+
+				null_ls.builtins.formatting.black,
+				require("none-ls.diagnostics.flake8"),
+
+				null_ls.builtins.formatting.prettierd,
+				require("none-ls.diagnostics.eslint_d"),
+
+				null_ls.builtins.formatting.clang_format,
+
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.formatting.goimports_reviser,
+				null_ls.builtins.formatting.golines,
+			},
+		})
+
+		require("mason-null-ls").setup({
+			ensure_installed = nil,
+			automatic_installation = true,
+			handlers = {},
 		})
 	end,
 }
